13cc9a7130d74823eea3061783dc29bd3774d905
AntoBach5 <antoaltmann25@gmail.com>
2025-12-15 18:31:02 +0100

    Project 15/12/2025

--- Square root.py
+++ Square root.py
@@ -1,1 +1,3 @@
-enter = int(input("Enter the number you want to find the square root from: "))
+number = int(input("Enter the number you want to find the square root from: "))
+square_root = number ** 0.5
+print(f"The square root of {number} is",square_root)
