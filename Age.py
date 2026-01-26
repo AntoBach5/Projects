bd510052034c01b0d951b0b643f8d370c3be623d
AntoBach5 <antoaltmann25@gmail.com>
2026-01-26 18:01:05 +0100

    Antonio

---
+++ Age.py
@@ -0,0 +1,6 @@
+age = int(input("Enter your age: "))
+
+if age >= 10 and age <= 20:
+    print(f"As you are {age} years old, you ARE allowed in the class")
+else:
+    print(f"As you are {age} years old, you are NOT allowed in the class")
