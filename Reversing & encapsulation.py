b4a204ca84e1632cf8331c915013722a783f8407
AntoBach5 <antoaltmann25@gmail.com>
2026-06-30 20:10:45 +0200

    Turning around and encapsulation

---
+++ Reversing & encapsulation.py
@@ -0,0 +1,9 @@
+class Reversing_String:
+    def __init__(self):
+        self.__default_string = "Codingal is awesome!!!"
+    
+    def __reversing(self):
+        print(self.__default_string[::-1])
+    
+final_string = Reversing_String()
+final_string._Reversing_String__reversing()
